e105359753122d273cc3f3801e78f638d78ecee1
moyo-1234 <folake.abudiore@gmail.com>
2025-09-21 19:23:19 +0100

    Add files via upload

--- rocket.py
+++ rocket.py
@@ -1,4 +1,5 @@
 import pygame
+from time import *
 pygame.init()
 run = True
 Screen = pygame.display.set_mode((714,400))
@@ -7,41 +8,14 @@ bimg = pygame.image.load(r"C:\Users\femia\Desktop\python_game_dev\Pro Game dev\s
 
 keys = [False,False,False,False]
 #up,right,down,left
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+rx = 357
+ry = 200
 
 
 
 while run == True:
     Screen.blit(bimg,(0,0))
-    Screen.blit(rimg,(357,200))
+    Screen.blit(rimg,(rx,ry))
     pygame.display.update()
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
@@ -49,5 +23,29 @@ while run == True:
         elif i.type == pygame.KEYDOWN:
             if i.key == pygame.K_UP:
                 keys[0] = True
-            if i.key == pygame.K_DOWN    
+            if i.key == pygame.K_DOWN:
+                keys[1] = True
+            if i.key == pygame.K_LEFT:
+                keys[2] = True
+            if i.key == pygame.K_RIGHT:
+                keys[3] = True
+        elif i.type == pygame.KEYUP:
+            if i.key == pygame.K_UP:
+                keys[0] = False
+            if i.key == pygame.K_DOWN:
+                keys[1] = False
+            if i.key == pygame.K_LEFT:
+                keys[2] = False
+            if i.key == pygame.K_RIGHT:
+                keys[3] = False
+    if keys[0] == True:
+        ry = ry - 1 
+    elif keys[1] == True:
+        ry = ry + .5
+    elif keys[2] == True:
+        rx = rx  - 1
+    elif keys[3] == True:
+        rx = rx + 1
+    ry = ry + .5
+    sleep(0.005)
 
